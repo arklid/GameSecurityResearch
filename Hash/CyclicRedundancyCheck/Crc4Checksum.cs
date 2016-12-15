ab6f9eafcbc293efdb6fdfe0264be8edf45c25a7
Fredric Baeckström <laptop@baeckstrom.com>
2016-12-15 20:13:22 +0100

    Added bunch of checksums.

---
+++ Hash/CyclicRedundancyCheck/Crc4Checksum.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// Copyright (c) 2016 Fredric Baeckström Arklid
+/// All rights reserved.
+/// 
+/// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// 
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+/// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// 
+/// 0101010001101000011001010111001001100101001000000110100101110011001000000110111001101111001000000111001101110000011011110110111101101110
+/// </summary>
+namespace GameSecurityResearch.Hash.CyclicRedundancyCheck
+{
+    public static class Crc4Checksum
+    {
+        private static readonly byte[] TabCrc4 = {
+            0, 3, 6, 5, 12, 15, 10, 9, 11, 8, 13, 14, 7, 4, 1, 2
+        };
+
+        /// <summary>
+        /// CRC-4 Checksum
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="length">Length</param>
+        /// <returns>Checksum</returns>
+        public static byte Crc4<T>(this T[] buffer, int length)
+        {
+            var crc = 0;
+
+            for (var k = 0; k < length; k++)
+            {
+                crc ^= (dynamic)buffer[k] >> 4;
+                crc = TabCrc4[crc];
+                crc ^= (dynamic)buffer[k] & 0xF;
+                crc = TabCrc4[crc];
+            }
+
+            return (byte)crc;
+        }
+    }
+}
