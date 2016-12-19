06792b747fbdc1d8fdca4ffb29632fc5b9721db1
Fredric Baeckström <laptop@baeckstrom.com>
2016-12-19 13:18:50 +0100

    Added Monster Hunter 4 Ultimate crypto keys for decrypting data.

---
+++ Cryptography/MonsterHunter4UltimateCrypto.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Copyright (c) 2016 Fredric Baeckström Arklid
+/// All rights reserved.
+/// 
+/// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// 
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+/// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// 
+/// 0101010001101000011001010111001001100101001000000110100101110011001000000110111001101111001000000111001101110000011011110110111101101110
+/// </summary>
+namespace GameSecurityResearch.Cryptography
+{
+    public static class MonsterHunter4UltimateCrypto
+    {
+        //@Todo: Add decrypt and encrypt methods.
+		// Monster Hunter 4 Ultimate save game files encrypted using Blowfish with the keys metioned below.
+		private static readonly string Key = "blowfish key iorajegqmrna4itjeangmb agmwgtobjteowhv9mope";
+		private static readonly string DLC_Key_EU = "AgK2DYheaCjyHGPB";
+		private static readonly string DLC_Key_JP =  "AgK2DYheaCjyHGP8";
+		private static readonly string DLC_Key_KR =  "AgK2DYheaOjyHGP8";
+		private static readonly string DLC_Key_TW =  "Capcom123 ";
+    }
+}
