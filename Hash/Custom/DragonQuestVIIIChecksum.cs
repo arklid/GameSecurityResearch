e9befd3635218097623126803f32927b631f7bae
Fredric Baeckström <laptop@baeckstrom.com>
2016-12-20 15:10:11 +0100

    Added Dragon Quest VIII custom checksum.

---
+++ Hash/Custom/DragonQuestVIIIChecksum.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Copyright (c) 2016 Fredric Baeckström Arklid
+/// All rights reserved.
+/// 
+/// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// 
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+/// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// 
+/// 0101010001101000011001010111001001100101001000000110100101110011001000000110111001101111001000000111001101110000011011110110111101101110
+/// </summary>
+namespace GameSecurityResearch.Hash.Custom
+{
+    /// <summary>
+    /// This calculates the custom hash for Dragon Quest VIII save games.
+    /// </summary>
+    public static class DragonQuestVIIIChecksum
+    {
+        public static ushort Calculate(byte[] buffer)
+        {
+            ushort hash = 0;
+
+            for (ushort i = 0, value; i < 0xB9; i++)
+            {
+                value = buffer[(i + 1) << 6];
+
+                if (value != 0xFF) hash += value;
+            }
+
+            return hash;
+        }
+    }
+}
