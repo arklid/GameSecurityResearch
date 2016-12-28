3c6c055ab59ed711709d68a5e0cd7b27b74182ef
Fredric Baeckström <laptop@baeckstrom.com>
2016-12-28 11:17:12 +0100

    Added custom checksum routine for FIFA 09 for Nintendo DS.

---
+++ Hash/Custom/Fifa09Checksum.cs
@@ -0,0 +1,37 @@
+﻿
+/// <summary>
+/// Copyright (c) 2016 Fredric Baeckström Arklid
+/// All rights reserved.
+/// 
+/// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// 
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+/// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/// 
+/// 0101010001101000011001010111001001100101001000000110100101110011001000000110111001101111001000000111001101110000011011110110111101101110
+/// </summary>
+namespace GameSecurityResearch.Hash.Custom
+{
+    public static class Fifa09Checksum
+    {
+        /// <summary>
+        /// Calculates the custom checksum for FIFA 09 Nintendo DS version
+        /// </summary>
+        /// <param name="data">Save game data</param>
+        /// <returns>Checksum</returns>
+        public static ushort Calculate(byte[] data)
+        {
+            ushort checksum = 0;
+
+            int i;
+            for (i = 0x00000020; i < 0x0000010C; i++)
+            {
+                checksum += (ushort)(data[i] * ((0xA3 + 0x69) - i));
+            }
+
+            return checksum;
+        }
+    }
+}
